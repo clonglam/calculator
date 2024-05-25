2814a484171d4d94208987d320f0fb0918ba5922
CLong Lam <86845927+clonglam@users.noreply.github.com>
2024-05-24 19:35:59 -0700

    Feat: updated Testing environment

--- src/features/calculator/components/Calculator/Calculator.test.tsx
+++ src/features/calculator/components/Calculator/Calculator.test.tsx
@@ -1,7 +1,6 @@
-import { act, render } from "@testing-library/react"
+import { render } from "@testing-library/react"
 import { describe, expect, it } from "vitest"
 import Calculator from "./Calculator"
-// import { useCalculatorStore } from "../../stores/use-calculator-store"
 
 describe("Calculator", () => {
   it("renders without errors", () => {
